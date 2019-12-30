1e58aba93c65f5e3a1fbc30dfd1c02f803055b7c
aidenhuang <aidenhuang@lexin.com>
2019-12-30 16:03:39 +0800

    feat: 添加test 和 chore

--- constants/commit-types.js
+++ constants/commit-types.js
@@ -1,44 +1,52 @@
 /*
  * @Author: Aiden
  * @Date: 2019-11-11 14:55:02
- * @LastEditors: Aiden
- * @LastEditTime: 2019-11-29 16:53:28
+ * @LastEditors  : Aiden
+ * @LastEditTime : 2019-12-30 16:02:40
  */
 module.exports = [
   {
     name: 'feat: 新特性',
-    value: 'feat'
+    value: 'feat',
   },
   {
     name: 'improvement: 改善当前特性',
-    value: 'improvement'
+    value: 'improvement',
   },
   {
     name: 'build: 构建项目',
-    value: 'build'
+    value: 'build',
   },
   {
     name: 'fix: 修改问题',
-    value: 'fix'
+    value: 'fix',
   },
   {
     name: 'cr: code review',
-    value: 'cr'
+    value: 'cr',
   },
   {
     name: 'refactor: 代码重构',
-    value: 'refactor'
+    value: 'refactor',
   },
   {
     name: 'docs: 文档修改',
-    value: 'docs'
+    value: 'docs',
   },
   {
     name: 'style: 样式修改',
-    value: 'style'
+    value: 'style',
   },
   {
     name: 'perf: 性能优化',
-    value: 'perf'
-  }
-]
+    value: 'perf',
+  },
+  {
+    name: 'test: 添加测试或者更改已有测试',
+    value: 'test',
+  },
+  {
+    name: 'chore: 其它杂事',
+    value: 'chore',
+  },
+];
